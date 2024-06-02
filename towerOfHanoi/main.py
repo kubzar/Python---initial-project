d84769be9ba9a9f79bec480bac985619b417ca51
Jakub Zarębski <jakub.pawel.zarebski@gmail.com>
2024-06-02 23:29:51 +0200

    Initial logic

--- towerOfHanoi/main.py
+++ towerOfHanoi/main.py
@@ -5,3 +5,13 @@ def main():
 if __name__ == '__main__':
     main()
 
+def hanoi(n, pile1, pile2, pile3):
+    if n == 1:
+        print(f"Move disc from {pile1} to {pile2}.")
+    else:
+        hanoi(n - 1, pile1, pile3, pile2)
+        print(f"Move disc from {pile1} to {pile2}.")
+        hanoi(n - 1, pile3, pile2, pile1)
+
+hanoi(3,"A","B","C")
+
